edbaee910378af6d3d28fdb0bca89edb44681426
Ad450 <adjeimanue@gmail.com>
2023-01-23 14:41:33 +0000

    microservices setup

---
+++ UserService/src/Server.ts
@@ -0,0 +1,32 @@
+import { join } from "path";
+import { Configuration, Inject } from "@tsed/di";
+import { PlatformApplication } from "@tsed/common";
+import "@tsed/platform-express"; // /!\ keep this import
+import "@tsed/ajv";
+import * as controllers from "./controllers/rest/controllerExporter";
+
+@Configuration({
+  acceptMimes: ["application/json"],
+  httpPort: process.env.PORT || 8083,
+  httpsPort: false, // CHANGE
+  componentsScan: false,
+  mount: {
+    "/rest": [controllers],
+  },
+  middlewares: [
+    "cors",
+    "cookie-parser",
+    "compression",
+    "method-override",
+    "json-parser",
+    { use: "urlencoded-parser", options: { extended: true } },
+  ],
+  exclude: ["**/*.spec.ts"],
+})
+export class Server {
+  @Inject()
+  protected app: PlatformApplication;
+
+  @Configuration()
+  protected settings: Configuration;
+}

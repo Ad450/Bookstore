f3aa5646290a57d21813e8b0bdc8d5e2219159c4
Ad450 <adjeimanue@gmail.com>
2023-02-01 15:02:49 +0000

    testing auth signing

--- bookservice/src/controllers/v1/BookController.ts
+++ bookservice/src/controllers/v1/BookController.ts
@@ -1,7 +1,7 @@
 import { Controller } from "@tsed/di";
-import { Get } from "@tsed/schema";
+import { Get, Post } from "@tsed/schema";
 
-@Controller("/books")
+@Controller("/")
 export class BookController {
   @Get("/")
   get() {
@@ -11,5 +11,26 @@ export class BookController {
   @Get("/all-books")
   getAllBooks() {
     return "this returns all books";
+  }
+
+  @Post("/login")
+  login() {
+    return {
+      access_token: {
+        aud: "https://your.krakend.io",
+        iss: "bookstore",
+        sub: "1234567890qwertyuio",
+        jti: "mnb23vcsrt756yuiomnbvcx98ertyuiop",
+        exp: 1735689600,
+      },
+      refresh_token: {
+        aud: "https://your.krakend.io",
+        iss: "https://your-backend",
+        sub: "1234567890qwertyuio",
+        jti: "mnb23vcsrt756yuiomn12876bvcx98ertyuiop",
+        exp: 1735689600,
+      },
+      exp: 1735689600,
+    };
   }
 }
